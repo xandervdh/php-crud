b4e70561e6c05fac74dea317addb31e182074adb
Victoria <victoria.sofianidou@gmail.com>
2020-11-02 15:42:40 +0100

    added create new, edit, delete button in overview

--- view/overview.php
+++ view/overview.php
@@ -4,10 +4,17 @@
 
         <div id="row">
 
+            <a href="http://php-crud.local/?page=new" class='btn btn-primary'>Create new</a>
+
             <?php foreach ($classes as $class) : ?>
 
+                <a href="http://php-crud.local/?page=edit" class='btn btn-primary'>Edit</a>
+                <a href="http://php-crud.local/?page=delete" class='btn btn-primary'>Delete</a>
+
             <p>Name:<?php echo $class->getName();?> </p>
+
             <p>Location:<?php echo $class->getLocation();?> </p>
+
             <p>Teacher:<?php echo $class->getTeacher()->getName();?> </p>
 
             <?php foreach ($class->getStudents() as $student){
