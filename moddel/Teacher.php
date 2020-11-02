ac73868138c3687a80a08f92dc225cb3f00ac3a1
xandervdh <xander-v.d.h@hotmail.com>
2020-11-02 11:22:00 +0100

    deleted id

--- moddel/Teacher.php
+++ moddel/Teacher.php
@@ -3,42 +3,20 @@
 
 class Teacher
 {
-    private int $id;
     private string $name;
     private string $email;
 
-    /**
-     * Teacher constructor.
-     * @param int $id
-     * @param string $name
-     * @param string $email
-     */
-    public function __construct(int $id, string $name, string $email)
+    public function __construct(string $name, string $email)
     {
-        $this->id = $id;
         $this->name = $name;
         $this->email = $email;
     }
 
-    /**
-     * @return int
-     */
-    public function getId(): int
-    {
-        return $this->id;
-    }
-
-    /**
-     * @return string
-     */
     public function getName(): string
     {
         return $this->name;
     }
 
-    /**
-     * @return string
-     */
     public function getEmail(): string
     {
         return $this->email;
