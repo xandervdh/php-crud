6a20705e8e988089cd00a875900df2d85cdb0926
xandervdh <xander-v.d.h@hotmail.com>
2020-11-02 13:22:20 +0100

    fixed the index.php

--- controller/ProfileController.php
+++ controller/ProfileController.php
@@ -1,14 +1,12 @@
 <?php
 declare(strict_types=1);
 
-require 'moddel/ClassLoader.php';
-require 'moddel/Connection.php';
 class ProfileController
 {
     public function render()
     {
         $connection = new Connection();
-        $id = $_GET['profile'];
+        $id = $_GET['user'];
         $class = $connection->getClass($id);
         $teacher = $connection->getTeacher($id);
         $students = $connection->getStudents($id);
--- moddel/ClassModel.php
+++ moddel/ClassModel.php
@@ -1,5 +1,5 @@
 <?php
-declare(strict_types=);
+declare(strict_types=1);
 
 class ClassModel
 {
