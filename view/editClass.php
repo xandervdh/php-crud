fd7816529999e8838d2d446f23c72fe51521491f
xandervdh <xander-v.d.h@hotmail.com>
2020-11-03 15:16:29 +0100

    fixed bugs with edit page

--- view/editClass.php
+++ view/editClass.php
@@ -5,10 +5,10 @@ include 'view/includes/header.php'
 
 <form action="" method="post">
     <label for="firstname">Class Name:</label>
-    <input type="text" name="classname" value="<?php echo $class['name'] ;?>">
+    <input type="text" name="classname" value="<?php echo $this->class['name'] ;?>">
     <span class="text-danger"><?php echo $classNameErrMess ;?></span>
     <label for="email">Location:</label>
-    <input type="text" name="location" value="<?php echo $class['location'] ;?>">
+    <input type="text" name="location" value="<?php echo $this->class['location'] ;?>">
     <span class="text-danger"><?php echo $locationErrMess ;?></span>
     <button type="submit">Submit</button>
 </form>
