1587c8079cddcc868294c89c199daf9d01faa932
Victoria <victoria.sofianidou@gmail.com>
2020-11-04 12:33:40 +0100

    created cards with bootstrap, added class link and link back to homepage

--- view/studentOverview.php
+++ view/studentOverview.php
@@ -4,31 +4,42 @@
 
     <div id="row">
 
-        <a href="http://crud.local/?page=new" class='btn btn-primary'>Create new</a>
+        <a href="http://crud.local/?create=student" class='btn btn-primary'>Create new</a>
 
         <?php foreach ($this->overview as $student) : ?>
 
-            <a href="http://crud.local/?edit=student&user=<?php echo $student['id'] ?>" class='btn btn-primary'>Edit</a>
+            <div class="card col-3">
 
-            <form method ="post">
-                <input type="submit" value="delete" name="action" class='btn btn-primary'>
-                <input type="hidden" name="id" value="<?php echo $student['id'] ?>">
-            </form>
+                <a href="http://crud.local/?edit=student&user=<?php echo $student['id'] ?>"
+                   class='btn btn-primary'>Edit</a>
 
-            <p>Name: <a href="http://crud.local/?profile=student&user=<?php echo $student['id'] ?>"><?php echo $student['name'];?></a></p>
+                <form method="post">
+                    <input type="submit" value="delete" name="action" class='btn btn-primary'>
+                    <input type="hidden" name="id" value="<?php echo $student['id'] ?>">
+                </form>
 
-            <p>Email:<?php echo $student['email'];?> </p>
+                <p>Name:
+                    <a href="http://crud.local/?profile=student&user=<?php echo $student['id'] ?>"><?php echo $student['name']; ?></a>
+                </p>
 
-            <?php $class = $this->connection->getClass($student['classes_id']);
-            echo "<a href='http://crud.local/?profile=class&user=" . $class['id'] . "'>" . $class['name'] . "</a><br>";
-            ?>
+                <p>Email: <?php echo $student['email']; ?> </p>
 
-            <?php $teacher = $this->connection->getTeacher($student['classes_id']);
-            echo "<a href='http://crud.local/?profile=teacher&user=" . $teacher['id'] . "'>" . $teacher['name'] . "</a><br>";
-            ?>
 
-            <hr>
-        <?php endforeach;?>
+                <?php $class = $this->connection->getClass($student['classes_id']);
+                echo "Class: " . "<a href='http://crud.local/?profile=class&user=" . $class['id'] . "'>" . $class['name'] . "</a><br>";
+                ?>
+
+                <?php $teacher = $this->connection->getTeacher($student['classes_id']);
+                echo "Teacher: " . "<a href='http://crud.local/?profile=teacher&user=" . $teacher['id'] . "'>" . $teacher['name'] . "</a><br>";
+                ?>
+
+                <hr>
+
+            </div>
+
+        <?php endforeach; ?>
+
+        <a class="btn btn-primary" href="http://crud.local/">back to homepage</a>
 
     </div>
 
